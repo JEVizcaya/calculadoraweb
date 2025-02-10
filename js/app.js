38fc2b2a7451fdd6abefcfe22e590ae4d205dcfd
JEVizcaya <jevizcaya@hotmail.com>
2025-02-10 17:55:00 +0100

    todos botones operativos

--- js/app.js
+++ js/app.js
@@ -1,28 +1,31 @@
 //$(".btn").click(function(){}); //funcion anonima
 //$(".btn").click(()=>{}) esto es lo mismo
+let btnPulsado,operacion,operador1=0,operador2=0,resultado;
+
 $(".btn").click(function () {
-    let btnPulsado = $(this).html();
-    let operador1 = 0, operador2 = 0, operacion;
-    let resuktado;
+    btnPulsado = $(this).html();
     switch (btnPulsado) {
+        case "CE":
+        $("#pantalla").val($("#pantalla").val().slice(0,-1));
+        break;
         case "C":
             operador1 = 0;
             operador2 = 0;
-            $("#pantalla").val("")
+            $("#pantalla").val("");
             break;
         case "0":
         case "1":
         case "2":
         case "3":
         case "4":
-        case "5":
+        case "5": 
         case "6":
-        case "7":
+        case "7": 
         case "8":
         case "9":
-            $("#pantalla").val($("#pantalla").val()+ btnPulsado);
+            $("#pantalla").val($("#pantalla").val() + btnPulsado);
             break;
-            case "+":
+        case "+":
         case "-":   
         case "*":
         case "/":
